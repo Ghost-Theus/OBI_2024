703830c46c6ec2737e4d90d81258caac135d111e
Matheus <136654851+Ghost-Theus@users.noreply.github.com>
2023-12-12 09:53:52 -0300

    Add files via upload

---
+++ torneio.py
@@ -0,0 +1,23 @@
+# https://olimpiada.ic.unicamp.br/pratique/pj/2021/f1/torneio/
+
+v = 0
+p = 0
+for i in range(6):
+    res = input()
+    if res == "V":
+        v += 1
+    else:
+        p += 1
+
+if v == 5 or v == 6:
+    print("1")
+elif v == 3 or v == 4:
+    print("2")
+elif v == 1 or v == 2:
+    print("3")
+else:
+    print("-1")
+
+
+
+
